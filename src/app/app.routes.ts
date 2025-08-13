c68352c7cfd2fe8304d63d3d391cac6ed26911fa
Yossy15 <gameimp72@gmail.com>
2025-08-13 23:37:33 +0700

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,11 +6,9 @@ import { PostsComponent } from './components/posts/posts.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { ToptenComponent } from './components/topten/topten.component';
-import { GraphComponent } from './graph/graph.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { MainComponent } from './setting/main/main.component';
 import { AddimagesComponent } from './setting/addimages/addimages.component';
-import { ToptennullComponent } from './components/toptennull/toptennull.component';
 import { EditimagesComponent } from './setting/editimages/editimages.component';
 import { ChImageComponent } from './setting/ch-image/ch-image.component';
 
@@ -21,8 +19,6 @@ export const routes: Routes = [
     { path: "login", component: LoginComponent },
     { path: "signup", component: SignupComponent },
     { path: "topten", component: ToptenComponent },
-    { path: "toptennull", component: ToptennullComponent },
-    { path: "graph", component: GraphComponent },
     { path: "dashboard", component: DashboardComponent },
     { path: "main", component: MainComponent},
     { path: "add-img", component: AddimagesComponent},
